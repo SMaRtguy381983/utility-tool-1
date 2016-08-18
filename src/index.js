a1fd79402c77733e4234963d9d2c24b506e58e5d
Dustin <dustin.zeigler@fullsail.edu>
2016-08-18 05:44:31 -0400

    Created src dir and index.js, which will serve as our modules core. Also brought the code over from appStore/src/libs/utilities.js. Made significant improments in readibility and removed repetitive code.

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+function write(str = '') {
+  process.stout.write(`${str}\n`);
+}
+
+exports.debug = (msg, obj = null, errLevel = 1, status = null) => {
+  // Check that DEBUG is true
+  if (process.env.DEBUG) {
+    // Define date
+    const date = new Date();
+    const ds = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    const d = date.getDay();
+    const h = date.getHours();
+    const hr = ((h + 11) % 12) + 1;
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    const suf = h < 12 ? 'AM' : 'PM';
+
+    // Make friendly date and message
+    const fDate = `${ds[d]} ${hr}:${m > 9 ? '' : '0'}${m}:${s > 9 ? '' : '0'}${s} ${suf}`;
+    const fMsg = `${status ? `${status} - ` : ''}${chalk.bold(msg)}`;
+
+    // Write a blank line to the console
+    write();
+
+    // Write fDate to the console
+    write(chalk.bgBlue.dim(fDate));
+
+    // Check if this is not an error
+    if (!errLevel) {
+      // Write fMsg -- using green background -- to the console
+      write(chalk.bgGreen(fMsg));
+    } else {
+      // Write fMsg -- using red background -- to the console
+      write(chalk.bgRed(fMsg));
+    }
+
+    // Check if obj is not empty
+    if (obj && Object.keys(obj).length) {
+      // Write obj to the console
+      write(chalk.bgYellow.black(JSON.stringify(obj, null, 2)));
+    }
+
+    // Write a blank line to the console
+    write();
+
+    // Log to file
+    let pretty = date + '\n';
+    pretty += (status ? status + ' - ' : '') + msg + '\n';
+    // Check if obj is not empty
+    if (obj && (obj.length > 0 || Object.keys(obj).length > 0)) {
+      pretty += JSON.stringify(obj, null, 2) + '\n';
+    }
+    pretty += '\n';
+    fs.appendFile('logs/console.log', pretty, (err) => {
+      if (err) {
+        write(`\n${chalk.red(err)}\n`);
+      }
+    });
+  }
+};
