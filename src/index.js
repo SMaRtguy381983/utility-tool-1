ebc4ac18f9241b3a2439de19ccd4ab479da1aeea
Dustin <dustin.zeigler@fullsail.edu>
2016-08-21 18:52:22 -0400

    Updated debug function to allow for null on all params and removed the fs file logger.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,64 @@
 const chalk = require('chalk');
-const fs = require('fs');
 
-exports.debug = (msg, obj = null, errLevel = 1, httpCode = null) => {
-  // Check that DEBUG is true
-  if (process.env.DEBUG) {
-    // Define date
+exports.debug = (msg = null, obj = null, errLevel = 1, httpCode = null) => {
+  if (msg || obj) {
+    // Define date and time.
     const date = new Date();
-    const ds = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-    const d = date.getDay();
-    const h = date.getHours();
-    const hr = ((h + 11) % 12) + 1;
-    const m = date.getMinutes();
-    const s = date.getSeconds();
-    const suf = h < 12 ? 'AM' : 'PM';
+    const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    const day = days[date.getDay()];
+    let hour = ((date.getHours() + 11) % 12) + 1;
+    const suff = hour < 12 ? 'AM' : 'PM';
+    hour = ((hour + 11) % 12) + 1;
+    let mins = date.getMinutes();
+    mins = `${mins > 9 ? '' : '0'}${mins}`;
+    let secs = date.getSeconds();
+    secs = `${secs > 9 ? '' : '0'}${secs}`;
 
-    // Make friendly date and message
-    const fDate = `${ds[d]} ${hr}:${m > 9 ? '' : '0'}${m}:${s > 9 ? '' : '0'}${s} ${suf}`;
-    const fMsg = `${httpCode ? `${httpCode} - ` : ''}${chalk.bold(msg)}`;
+    // Define friendly date and message.
+    const fDate = `${day} ${hour}:${mins}:${secs} ${suff}`;
+    let fMsg = '';
 
-    // Log to file
-    let pretty = date + '\n';
-    pretty += (httpCode ? httpCode + ' - ' : '') + msg + '\n';
-    // Check if obj is not empty
-    if (obj && (obj.length > 0 || Object.keys(obj).length > 0)) {
-      pretty += JSON.stringify(obj, null, 2) + '\n';
-    }
-    pretty += '\n';
-    fs.appendFile('logs/console.log', pretty, (err) => {
-      if (err) {
-        console.log(chalk.bgRed.bold('Error while writing to logs/console.log...'));
-        console.log(chalk.bgRed(err));
-        console.log();
-      }
-    });
-
-    // Write a blank line to the console
+    // Write a blank line to the console.
     console.log();
 
-    // Write fDate to the console
+    // Write fDate to the console.
     console.log(chalk.bgBlue.dim(fDate));
 
-    // Check if this is not an error
-    if (!errLevel) {
-      // Write fMsg -- using green background -- to the console
-      console.log(chalk.bgGreen(fMsg));
-    } else {
-      // Write fMsg -- using red background -- to the console
-      console.log(chalk.bgRed(fMsg));
-    }
-
-    // Build a reponse for each call
+    // Build a reponse for each call.
     const res = {
       date: fDate,
-      msg: fMsg,
     };
 
-    // Check if obj is not empty
-    if (obj && Object.keys(obj).length) {
-      // Write obj to the console
-      console.log(chalk.bgYellow.black(JSON.stringify(obj, null, 2)));
+    // Check that msg is not null.
+    if (msg && msg === String(msg) && msg.length) {
+      // Add msg to the response.
+      res.msg = msg;
 
-      // Add obj to the response
+      if (httpCode) {
+        fMsg += `${httpCode} - `;
+
+        res.httpCode = httpCode;
+      }
+
+      fMsg += chalk.bold(msg);
+
+      // Check if this is not an error.
+      if (!errLevel) {
+        // Write fMsg -- using green background -- to the console.
+        console.log(chalk.bgGreen(fMsg));
+      } else {
+        // Write fMsg -- using red background -- to the console.
+        console.log(chalk.bgRed(fMsg));
+      }
+    }
+
+    // Check that obj is not null.
+    if (obj && obj === Object(obj)) {
+      // Add obj to the response.
       res.obj = obj;
+
+      // Write obj to the console.
+      console.log(chalk.bgYellow.black(JSON.stringify(obj, null, 2)));
     }
 
     // Write a blank line to the console
